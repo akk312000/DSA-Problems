2a65d38d7e9916358c62e83b4b379cfa9e79c6e9
akk312000 <akk312000@gmail.com>
2021-08-06 16:11:14 +0000

    Sync LeetCode submission - Next Permutation (cpp)

--- Leetcode/problems/next_permutation/solution.cpp
+++ Leetcode/problems/next_permutation/solution.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-       next_permutation(nums.begin(),nums.end());
+        int n=nums.size();
+        if(n<=1)return;
+        int i;
+        for( i=n-1;i>0;i--)
+        {
+            if(nums[i]>nums[i-1])break;
+        }
+        if(i==0)
+        {
+            reverse(nums.begin(),nums.end());
+            return;
+            
+        }
+        int k;
+        for(k=n-1;k>i;k--)
+        {
+            if(nums[k]>nums[i-1])break;
+        }
+        swap(nums[k],nums[i-1]);
+        reverse(nums.begin()+i,nums.end());
+        
+        
     }
 };
