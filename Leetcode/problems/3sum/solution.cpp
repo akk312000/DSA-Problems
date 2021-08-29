c42e5bbc4c8b63467deabcc38c7bd3ce8076eba6
akk312000 <akk312000@gmail.com>
2021-08-29 02:00:13 +0000

    Sync LeetCode submission - 3Sum (cpp)

--- Leetcode/problems/3sum/solution.cpp
+++ Leetcode/problems/3sum/solution.cpp
@@ -1,51 +1,53 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-        vector<vector<int>>vp;
         int n=nums.size();
         sort(nums.begin(),nums.end());
+        set<vector<int>>ans;
+        vector< vector<int>>finalans;
+         if(n<3)
+         {
+             return finalans;
+         }
         for(int i=0;i<n-2;i++)
         {
-            if(i==0||(nums[i]!=nums[i-1]))
+              int target=-nums[i];
+            if(i==0||nums[i]!=nums[i-1])
             {
-                   int target=-nums[i];
-            
-                int low=i+1;
-                int end=n-1;
-                while(low<end)
+                
+            int j=i+1;
+            int k=n-1;
+            while(j<k)
+            {
+               if(nums[j]+nums[k]==target)
+               {
+                   vector<int>temp;
+                   temp.push_back(nums[i]);
+                   temp.push_back(nums[j]);
+                   temp.push_back(nums[k]);
+                                   while(j<k&&nums[k]==nums[k-1])
+                   {k--;}
+                   while(j<k&&nums[j]==nums[j+1])
+                   {j++;}
+                   finalans.push_back(temp);
+                   j++;
+                   k--;
+                   
+                  
+               }
+                else if(nums[j]+nums[k]>target)
                 {
-                    if(nums[low]+nums[end]==target)
-                    {
-                       vector<int>newvec;
-                        newvec.push_back(nums[i]);
-                        newvec.push_back(nums[low]);
-                        newvec.push_back(nums[end]);
-                        vp.push_back(newvec);
-                        while(low<end&&nums[low]==nums[low+1])
-                        {
-                            low++;
-                        }
-                        while(low<end&&nums[end]==nums[end-1])
-                        {
-                            end--;
-                        }
-                        low++;end--;
-                    }
-                    else if(nums[low]+nums[end]<target)
-                    {
-                        low++;
-                    }
-                    else
-                    {
-                        end--;
-                    }
+                    k--;
                 }
+                else
+                {
+                    j++;
+                }
+
             }
-            
+            }
         }
         
-        return vp;
-        
+        return finalans;
     }
 };
