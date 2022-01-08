f0bd0e5f538e304d819f7ff951d6e20edf44cd13
akk312000 <akk312000@gmail.com>
2022-01-08 08:42:38 +0000

    Sync LeetCode submission - Sqrt(x) (cpp)

--- Leetcode/problems/sqrt(x)/solution.cpp
+++ Leetcode/problems/sqrt(x)/solution.cpp
@@ -1,25 +1,6 @@
 class Solution {
 public:
     int mySqrt(int x) {
-       
-         int start=1; int end=INT_MAX-1;
-       
-        while(start<=end)
-            
-        {  int mid=(start)+(end-start)/2;
-            if(mid==x/mid)
-            {
-                return mid;
-            }
-            else if(mid<x/mid)
-            {
-                start=mid+1;
-            }
-            else
-            {
-                end=mid-1;
-            }
-        }
-        return start-1;
+       return sqrt(x); 
     }
 };
