6d7fbb9e234c97e0f689521504db6bd09718dcf8
akk312000 <akk312000@gmail.com>
2022-01-24 06:34:16 +0000

    Sync LeetCode submission - Subsets (cpp)

---
+++ Leetcode/problems/subsets/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        int n=nums.size();
+        int tot=1<<n;
+        for(int i=0;i<tot;i++)
+        {vector<int>tp;
+            for(int j=0;j<n;j++)
+            {
+                if(1<<j&i)
+                {
+                    tp.push_back(nums[j]);
+                }
+            }
+         ans.push_back(tp);
+        }
+        return ans;
+    }
+};
