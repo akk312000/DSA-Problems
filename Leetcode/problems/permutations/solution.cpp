3598352553c312261459b590862e155d591715ef
akk312000 <akk312000@gmail.com>
2022-05-17 15:35:07 +0000

    Sync LeetCode submission - Permutations (cpp)

--- Leetcode/problems/permutations/solution.cpp
+++ Leetcode/problems/permutations/solution.cpp
@@ -1,26 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        
-        vector<vector<int>>vp;
-        returnpre(0,nums,vp);
-    return vp;
-    }
-    void returnpre(int begin,vector<int>&nums,vector<vector<int>>&vp)
-    {
-        if(begin>=nums.size())
+        vector<vector<int>>ans;
+                set<vector<int>>sp;
+
+        while(1)
         {
-            vp.push_back(nums);
-            
-        }
-        else
+        int n=nums.size();
+       int j=n-1;
+        for(int i=n-1;i>0;i--)
         {
-            for(int i=begin;i<nums.size();i++)
+            if(nums[i-1]<nums[i])
             {
-                swap(nums[i],nums[begin]);
-                 returnpre(begin+1,nums,vp);
-                swap(nums[i],nums[begin]);
+                j=i-1;break;
             }
         }
+        if(j==n-1)
+        {
+            reverse(nums.begin(),nums.end());
+           
+        }
+            else{
+        int k=0;
+        for(int i=n-1;i>0;i--)
+        {
+            if(nums[i]>nums[j])
+            {
+                k=i;break;
+            }
+        }
+        swap(nums[k],nums[j]);
+        reverse(nums.begin()+j+1,nums.end());}
+                        sp.insert(nums);
+
+            ans.push_back(nums);
+                        if(ans.size()>sp.size()){ans.pop_back();break;}
+
+        }
+        return ans;
     }
+    
 };
