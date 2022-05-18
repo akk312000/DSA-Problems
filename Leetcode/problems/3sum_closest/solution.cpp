5b2fb643dd774d17a8cf07761efe6464d1b5304a
akk312000 <akk312000@gmail.com>
2022-05-18 03:52:07 +0000

    Sync LeetCode submission - 3Sum Closest (cpp)

--- Leetcode/problems/3sum_closest/solution.cpp
+++ Leetcode/problems/3sum_closest/solution.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    int threeSumClosest(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int diff=INT_MAX;
-        int ans=nums[0]+nums[1]+nums[2];
+	int threeSumClosest(vector<int>& nums, int target) {
+		sort(nums.begin(),nums.end());
+		int n=nums.size();int diffmax=INT_MAX;int ans=nums[0]+nums[1]+nums[2];
         for(int i=0;i<n-2;i++)
         {
             int j=i+1;
             int k=n-1;
-            while(j<k){
-                int sumo=nums[i]+nums[j]+nums[k];
-                int difo=abs(sumo-target);
-                if(difo<diff){
-                    diff=difo;
-                    ans=sumo;
+            while(j<k)
+            {
+                int sum=nums[i]+nums[j]+nums[k];
+                int diff=abs(target-sum);
+                if(diff<diffmax)
+                {
+                    diffmax=diff;
+                    ans=sum;
                 }
-                if(sumo>target)
+                if(sum>target)
                 {
                     k--;
                 }
                 else j++;
             }
-
         }
         return ans;
-    }
-};
+		
+			
+			}
+		};
