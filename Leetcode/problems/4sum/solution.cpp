82e06a14cd0774de5a77e726b88b909ed3f5b6e3
akk312000 <akk312000@gmail.com>
2022-05-26 17:26:43 +0000

    Sync LeetCode submission - 4Sum (cpp)

--- Leetcode/problems/4sum/solution.cpp
+++ Leetcode/problems/4sum/solution.cpp
@@ -3,52 +3,43 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
-        set<vector<int>>sp;
+                set<vector<int>>ans;
+
+        vector<vector<int>>sp;
         for(int i=0;i<n-3;i++)
         {
-            for(int j=i+1;j<n-2;j++)
-            {
-                
-                int start=j+1;int end=n-1;
-                int kp=target-(nums[i]+nums[j]);
-                while(start<end)
-                {
-                    int sum=(nums[start]+nums[end]);
-                    if(sum==kp)
-                    {
-                        vector<int>vp;
-                        vp.push_back(nums[i]);
-                        vp.push_back(nums[j]);
-                        vp.push_back(nums[start]);
-                        vp.push_back(nums[end]);
-                        sp.insert(vp);
-                        while(start<end&&nums[start]==nums[start+1])
-                        {
-                            start++;
-                        }
-                        while(start<end&&nums[end]==nums[end-1])
-                        {
-                            end--;
-                        }
-                        start++;end--;
-                    }
-                    else if(sum<kp)
-                    {
-                        start++;
-                    }
-                    else
-                    {
-                        end--;
-                    }
-                }
-                
-            }
+           for(int j=i+1;j<n-2;j++)
+           {
+               int k=j+1;
+               int l=n-1;
+               while(k<l)
+               {
+                   if(target-nums[i]-nums[l]==nums[j]+nums[k])
+                   {
+                       vector<int>vp;
+                       vp.push_back(nums[i]);
+                                              vp.push_back(nums[j]);
+
+                                              vp.push_back(nums[k]);
+
+                                              vp.push_back(nums[l]);
+                       ans.insert(vp);k++;
+
+                   }
+                   else if(target-nums[i]-nums[l]>nums[j]+nums[k])
+                   {
+                       k++;
+                   }
+                   else l--;
+               }
+               while(j+1<n&&nums[j+1]==nums[j])j++;
+           }
+            while(i+1<n&&nums[i+1]==nums[i])i++;
         }
-        for(auto it:sp)
+        for(auto it:ans)
         {
-            ans.push_back(it);
+            sp.push_back(it);
         }
-        return ans;
+        return sp;
     }
 };
