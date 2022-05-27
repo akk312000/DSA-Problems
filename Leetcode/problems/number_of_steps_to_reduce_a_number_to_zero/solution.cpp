1d1be8f38f18ee93c6e7b3bb0d345ad995b10682
akk312000 <akk312000@gmail.com>
2022-05-27 17:41:21 +0000

    Sync LeetCode submission - Number of Steps to Reduce a Number to Zero (cpp)

---
+++ Leetcode/problems/number_of_steps_to_reduce_a_number_to_zero/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numberOfSteps(int num) {
+        int ans=0;
+        while(num)
+        {
+            if(num%2==0)num/=2;
+            else num--;
+            ans++;
+        }
+        return ans;
+    }
+};
