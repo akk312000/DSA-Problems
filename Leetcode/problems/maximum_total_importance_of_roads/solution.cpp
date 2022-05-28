4f2666a5bd7d2db3c2da9ae5d304d706f7519600
akk312000 <akk312000@gmail.com>
2022-05-28 17:07:07 +0000

    Sync LeetCode submission - Maximum Total Importance of Roads (cpp)

---
+++ Leetcode/problems/maximum_total_importance_of_roads/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long maximumImportance(int n, vector<vector<int>>& roads) {
+        long long cnt[500001]={},res=0;
+        for(auto &r:roads)
+        {
+            ++cnt[r[0]];
+            ++cnt[r[1]];
+        }
+        sort(begin(cnt),begin(cnt)+n);
+        for(int i=0;i<n;i++)
+        {
+            res+=cnt[i]*(i+1);
+        }
+        return res;
+    }
+};
