c23b5adc8e7eee7a5561e48899c9535de4e70fd8
akk312000 <akk312000@gmail.com>
2022-06-01 09:03:02 +0000

    Sync LeetCode submission - K-diff Pairs in an Array (cpp)

---
+++ Leetcode/problems/k-diff_pairs_in_an_array/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+         ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(0);
+        unordered_map<int,int> a;
+        for(int i:nums)
+            a[i]++;
+        int ans=0;
+        for(auto x:a)
+        {
+            if(k==0)
+            {    
+                if(x.second>1)
+                ans++;
+            }
+             else if(a.find(x.first+k)!=a.end())
+                ans++;
+        }
+        
+        return ans;
+    }
+};
