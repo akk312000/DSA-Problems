de993611b6879e2dcfa1fe1491ca0875604f46ba
akk312000 <akk312000@gmail.com>
2022-06-06 16:43:00 +0000

    Sync LeetCode submission - Jump Game (cpp)

---
+++ Leetcode/problems/jump_game/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n=nums.size();
+        int target=n-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(i+nums[i]>=target)
+            {
+                target=i;
+            }
+        }
+        if(target==0)
+        {
+            return true;
+        }
+        return false;
+    }
+};
